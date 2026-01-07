e983adc602ed2ddd798b0edeadc8015a1022bce1
Jack <109554692+SamuraiJack96@users.noreply.github.com>
2026-01-07 12:45:31 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/eventlistener/main.go
@@ -0,0 +1,23 @@
+// cmd/eventlistener/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "eventlistener/internal/eventlistener"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := eventlistener.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
